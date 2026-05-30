749f15df1068f9b1aa84fcbb0768a57bb6d94f59
Jay-r Sora <otchok.09052005@email.com>
2026-05-30 08:55:13 +0800

    Added Save Functions

--- Model.py
+++ Model.py
@@ -9,6 +9,15 @@ def load_students(self):
     except FileNotFoundError:
         self.students = []
 
+        def save_students(self):
+    with open(self.FILE_NAME, "w") as file:
+        json.dump(
+            [{"id": s.id, "name": s.name, "age": s.age} for s in self.students],
+            file,
+            indent=4
+        )
+        
+
 class Student:
     def __init__(self, student_id, name, age):
         self.id = student_id
